f0872549d04aa2b7cd4f287d7f90a241e64d9b59
Victor Nunes <nunessvictorr@gmail.com>
2022-05-31 16:07:52 -0300

    Fix missing getters and setters

--- src/main/java/br/edu/ifto/pweb/ecommerce/model/entity/ClientePessoaFisica.java
+++ src/main/java/br/edu/ifto/pweb/ecommerce/model/entity/ClientePessoaFisica.java
@@ -17,5 +17,38 @@ public class ClientePessoaFisica extends Cliente {
     private String email;
 
     @NotBlank(message = "Esse campo é obrigatório")
+    @Size(min = 11, max = 11, message = "Esse campo exige {max} caracteres")
     private String telefone;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
