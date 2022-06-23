7c9bd9ac90215e0747925ac1b4338c40d14b959d
Victor Nunes <nunessvictorr@gmail.com>
2022-06-23 17:27:53 -0300

    Add filter by categoriaId

--- src/main/java/br/edu/ifto/pweb/ecommerce/controller/PrecoController.java
+++ src/main/java/br/edu/ifto/pweb/ecommerce/controller/PrecoController.java
@@ -1,14 +1,14 @@
 package br.edu.ifto.pweb.ecommerce.controller;
 
 import br.edu.ifto.pweb.ecommerce.model.entity.Preco;
+import br.edu.ifto.pweb.ecommerce.model.entity.Produto;
+import br.edu.ifto.pweb.ecommerce.model.repository.CategoriaRepository;
 import br.edu.ifto.pweb.ecommerce.model.repository.PrecoRepository;
 import br.edu.ifto.pweb.ecommerce.model.repository.ProdutoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,11 +23,14 @@ import java.util.Optional;
 @RequestMapping("precos")
 public class PrecoController implements ModelController<Preco, Long> {
     private final PrecoRepository repository;
+    private final CategoriaRepository categoriaRepository;
     private final ProdutoRepository produtoRepository;
 
-    @Autowired
-    public PrecoController(PrecoRepository repository, ProdutoRepository produtoRepository) {
+    public PrecoController(PrecoRepository repository,
+                           CategoriaRepository categoriaRepository,
+                           ProdutoRepository produtoRepository) {
         this.repository = repository;
+        this.categoriaRepository = categoriaRepository;
         this.produtoRepository = produtoRepository;
     }
 
@@ -42,7 +45,17 @@ public class PrecoController implements ModelController<Preco, Long> {
     @Override
     @GetMapping("/list")
     public ModelAndView list(ModelMap modelMap) {
+        modelMap.addAttribute("categorias", categoriaRepository.findAll());
         modelMap.addAttribute("precos", repository.findAll());
+        return new ModelAndView("/precos/list", modelMap);
+    }
+
+    @GetMapping("/filter/categoria/{id}")
+    public ModelAndView filterByCategoria(@PathVariable String id, ModelMap modelMap) {
+        modelMap.addAttribute("categoriaId", Long.valueOf(id));
+        modelMap.addAttribute("categorias", categoriaRepository.findAll());
+        modelMap.addAttribute("precos", repository.findAllByProduto_Categorias_Id(Long.valueOf(id)));
+
         return new ModelAndView("/precos/list", modelMap);
     }
 
--- src/main/java/br/edu/ifto/pweb/ecommerce/model/repository/PrecoRepository.java
+++ src/main/java/br/edu/ifto/pweb/ecommerce/model/repository/PrecoRepository.java
@@ -4,8 +4,11 @@ import br.edu.ifto.pweb.ecommerce.model.entity.Preco;
 import br.edu.ifto.pweb.ecommerce.model.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PrecoRepository extends JpaRepository<Preco, Long> {
     Optional<Preco> findFirstByActiveTrueAndProdutoOrderByUpdatedAtDesc(Produto produto);
+
+    List<Preco> findAllByProduto_Categorias_Id(Long produto_categorias_id);
 }
